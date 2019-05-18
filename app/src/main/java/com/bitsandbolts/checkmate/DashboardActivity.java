badd92bc7c1e7b52d3999b536365eb577e5901f6
Wong Jin Fu Shaun <sw_best@hotmail.com>
2019-05-18 20:32:27 +0800

    redone login page halfway

---
+++ app/src/main/java/com/bitsandbolts/checkmate/DashboardActivity.java
@@ -0,0 +1,64 @@
+package com.bitsandbolts.checkmate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        // get current Auth state
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            //if No user signed in, it will direct to Login activity
+            Intent intent = new Intent(DashboardActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else Toast.makeText(this,"Welcome to Firebase Authentication",Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        //menu.xml is inflated in this activity
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        //on click on menu option "Logout"
+        if(item.getItemId() == R.id.logout){
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(DashboardActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        return true;
+    }
+
+
+}
--- app/src/main/java/com/bitsandbolts/checkmate/RegistrationActivity.java
+++ app/src/main/java/com/bitsandbolts/checkmate/RegistrationActivity.java
@@ -1,13 +1,70 @@
 package com.bitsandbolts.checkmate;
-
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bitsandbolts.checkmate.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    EditText userEmail, userPassword;
+    Button registerButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //This will get the Instance of Current  Auth state
+        mAuth = FirebaseAuth.getInstance();
+
+        userEmail = (EditText) findViewById(R.id.userEmail_editText);
+        userPassword = (EditText) findViewById(R.id.userPassword_editText);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Email = userEmail.getText().toString();
+                String Password = userPassword.getText().toString();
+
+                registerUser(Email, Password);
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+
+        //This method will create new User on firebase console...
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    // SignUp in success, Direct to the dashboard Page...
+                    Toast.makeText(RegistrationActivity.this, "Authentication Successfully completed", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(RegistrationActivity.this, "Authentication failed User already exits",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
